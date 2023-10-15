9186d51e3132856e59434cd111fc58bb85f7d13a
Anthony Alexander Atama <100134398+alexanderatama1@users.noreply.github.com>
2023-10-15 22:25:23 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int num;
+Console.Write("Enter a number; ");
+num = int.Parse(Console.ReadLine());
+
+if (num == 0)
+{
+    Console.WriteLine("Zero");
+}
+else if (num % 2 == 0)
+{
+    Console.WriteLine("Even");
+}
+else
+{
+    Console.WriteLine("Odd");
+}
